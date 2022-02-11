3488f3fe74b27f81cd7eb6b77b8f95e4369082b7
Mitchell Kotler <mitch@mitchellkotler.net>
2022-02-11 14:43:30 -0500

    sidekick script

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 """
-This is a hello world plugin for DocumentCloud
-
-It demonstrates how to write a plugin which can be activated from the
-DocumentCloud plugin system and run using Github Actions.  It receives data
-from DocumentCloud via the request dispatch and writes data back to
-DocumentCloud using the standard API
+This Add-On allows for easy calling of SideKick API from DocumentCloud
 """
 
 import json
+import re
 import sys
+import time
 
 import documentcloud
 
@@ -34,26 +31,78 @@ def load_params():
 def init():
     """Load the paraneters and initialize the DocumentCloud client."""
     token, base_uri, documents, query, params = load_params()
-    kwargs = {'base_uri': base_uri} if base_uri is not None else {}
+    kwargs = {"base_uri": base_uri} if base_uri is not None else {}
     client = documentcloud.DocumentCloud(**kwargs)
     client.session.headers.update({"Authorization": "Bearer {}".format(token)})
     client.session.headers["User-Agent"] += " (DC AddOn)"
     return client, documents, query, params
 
 
-def main(client, documents, query, params):
-    """The main plugin functionality goes here."""
-    # fetch your plugin specific data
-    name = params['data'].get("name", "world")
+def parse_project(query):
+    pattern = re.compile(r"project:[\w-]+-(\d+)")
+    match = query.search(pattern)
+    if not match:
+        return None
+    return int(match.group(1))
 
-    # add a hello note to the first page of each selected document
-    if documents:
-        for doc_id in documents:
-            document = client.documents.get(doc_id)
-            document.annotations.create(f"Hello {name}!", 0)
-    elif query:
-        for document in client.documents.search(query):
-            document.annotations.create(f"Hello {name}!", 0)
+
+def initialize_sidekick(client, project_id):
+    """Initialize the sidekick instance if it does not exist"""
+    # create the sidekick on the project
+    response = client.post(f"projects/{project_id}/sidekick/")
+
+    # check the status
+    response = client.get(f"projects/{project_id}/sidekick/")
+    status = response.json()["status"]
+    print(status)
+
+    # keep checking the status until it succeeds
+    while status == "pending":
+        # wait one minute before checking again
+        time.sleep(30)
+        response = client.get(f"projects/{PROJECT_ID}/sidekick/")
+        status = response.json()["status"]
+        print(status)
+
+
+def main(client, documents, query, params):
+    """Initialize SideKick if necessary and run learning routine"""
+
+    if "tag_name" not in params["data"]:
+        return
+
+    tag_name = params["data"]["tag_name"]
+    initialize = params["data"].get("initialize", False)
+    project_id = parse_project(query)
+
+    if initialize:
+        # allow the user to specify to force initialization
+        initialize_sidekick(client, project_id)
+    else:
+        response = client.get(f"projects/{project_id}/sidekick/")
+        if response.status_code == 404:
+            # if sidekick instance is not found, we will intialize it
+            initialize_sidekick(client, project_id)
+
+    # do the learning!
+    response = client.post(
+        f"projects/{project_id}/sidekick/learn/", data={"tagname": tag_name}
+    )
+    print(response.status_code)
+    print(response.json())
+
+    # check the status
+    response = client.get(f"projects/{project_id}/sidekick/")
+    status = response.json()["status"]
+    print(status)
+
+    # keep checking the status until it succeeds
+    while status == "pending":
+        # wait ten seconds before checking again
+        time.sleep(10)
+        response = client.get(f"projects/{project_id}/sidekick/")
+        status = response.json()["status"]
+        print(status)
 
 
 if __name__ == "__main__":
